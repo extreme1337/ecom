30853b61d25168e0f6831eebc97cc5ab36832809
Marko Miseljic <marko.miseljic.14@gmail.com>
2019-06-24 13:36:37 +0200

    Added reports for orders
    Added report which in database showing which product is ordered

--- resources/cart.php
+++ resources/cart.php
@@ -1,4 +1,4 @@
-<?php require_once("../resources/config.php"); ?>
+<?php require_once("config.php"); ?>
 
 
 <?php 
@@ -101,6 +101,57 @@ function show_paypal(){
         DELIMETER;
         echo $paypal_button;
     }
+}
+
+
+
+
+
+
+function reports(){
+    if(isset($_GET['tx'])){
+        $amount = $_GET['amt'];
+        $currency = $_GET['cc'];
+        $transaction = $_GET['tx'];
+        $status = $_GET['st'];
+        
+    
+    
+        $total = 0;
+        $item_quantity = 0;
+        foreach($_SESSION as $name => $value){
+            if($value > 0){
+                if(substr($name,0,8) == "product_"){
+                    $length = strlen(is_int($name) - 8);
+                    $id = substr($name, 8, $length);
+                    
+                    $send_order = query("INSERT INTO orders (order_amount,order_transaction,order_status,order_currency) VALUES('{$amount}','{$transaction}','{$status}','{$currency}')");
+                    $last_id = last_id();
+                    confirm($send_order);
+                    
+                    $query = query("SELECT * FROM products WHERE product_id=".escape_string($id)." ");
+                    confirm($query);
+                    while($row = fetch_array($query)){
+                        $item_quantity +=$value;
+                        $sub = $row['product_price']*$value;
+                        $product_price=$row['product_price'];
+                        $product_title=$row['product_title'];
+
+                        $insert_report = query("INSERT INTO reports (product_id,order_id,product_price,product_title,product_quantity) VALUES('{$id}','{$last_id}','{$product_price}','{$product_title}','{$value}')");
+                        confirm($insert_report);
+
+                    }
+                    $total += $sub;
+                    $item_quantity;
+                }
+            }
+
+        }
+        session_destroy();
+    }else{
+        redirect("index.php");
+    }
+    
 }
 
 
--- resources/functions.php
+++ resources/functions.php
@@ -1,6 +1,11 @@
 <?php 
 
 //helper functions
+function last_id(){
+    global $connection;
+    return mysqli_insert_id($connection);
+}
+
 function set_message($msg){
     if(!empty($msg)){
         $_SESSION['message']=$msg;
